9a1d3c138d68e27435c75f129f9c0d51dce1ce00
yuriikoval1997 <yuriikoval1997@gmail.com>
2020-02-27 11:04:02 +0200

    Deleted unused import

--- src/main/java/edu/yuriikoval1997/springjdbcexp/SpringJdbcExpApplication.java
+++ src/main/java/edu/yuriikoval1997/springjdbcexp/SpringJdbcExpApplication.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringJdbcExpApplication implements CommandLineRunner {
